3894b6d6081a1acee0c7880a78eccad3aa660672
Eduardo Luiz <eduardo.luiz3412@gmail.com>
2026-09-07 23:48:37 -0300

    feat (checkpoint-finalizado): funções chamadas na main e refactor nas últimas variáveis

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -5,7 +5,7 @@ import br.com.fiapride.model.veiculo;
 public class SistemaPrincipal {
     public static void main(String[] args) {
 
-        veiculo veiculo1 = new veiculo("Carlos","ABC-1234",-10,55);
+        veiculo veiculo1 = new veiculo("Carlos","ABC-1234",40,60);
 
         // cantinho da vergonha do código original, logo abaixo deste lindo constructor
         /*v1.individuo = "Carlos";
@@ -14,6 +14,27 @@ public class SistemaPrincipal {
         v1.abastecerGasolina(50);
         v1.consumirGasolina(100); // PERIGO: Consome mais que o disponível!*/
 
-        System.out.println("Dono: " + veiculo1.getIndividuo() + " | Placa: " + veiculo1.getPlaca() + " | Gasolina: " + veiculo1.getLitrosGasolina());
+        System.out.println("\nDono: " + veiculo1.getDono() + " | Placa: " + veiculo1.getPlaca() + " | Gasolina: " + veiculo1.getLitrosGasolina() + " L | Litragem tanque: " + veiculo1.getLitrosTanqueGasolina() + " L");
+
+        veiculo1.abastecerGasolina(25);
+
+        System.out.println("\nDono: " + veiculo1.getDono() + " | Placa: " + veiculo1.getPlaca() + " | Gasolina: " + veiculo1.getLitrosGasolina() + " L | Litragem tanque: " + veiculo1.getLitrosTanqueGasolina() + " L");
+
+        veiculo1.consumirGasolina(176);
+
+        System.out.println("\nDono: " + veiculo1.getDono() + " | Placa: " + veiculo1.getPlaca() + " | Gasolina: " + veiculo1.getLitrosGasolina() + " L | Litragem tanque: " + veiculo1.getLitrosTanqueGasolina() + " L");
+
+        // Try-catch's rápidos só para testar as condições das funções
+        try {
+            veiculo1.abastecerGasolina(-50);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+
+        try {
+            veiculo1.consumirGasolina(-10);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/br/com/fiapride/model/veiculo.java
+++ src/br/com/fiapride/model/veiculo.java
@@ -2,32 +2,73 @@ package br.com.fiapride.model;
 
 public class veiculo {
 
-    private String individuo;
+    private String dono;
     private String placa;
     private double litrosGasolina;
-    private double litrosTanqueCombustivel;
+    private double litrosTanqueGasolina;
 
-    public veiculo(String individuo, String placa, double gasolina, double litrosTanqueCombustivel) {
-        this.individuo = individuo;
+    public veiculo(String dono, String placa, double litrosGasolina, double litrosTanqueGasolina) {
+        this.dono = dono;
         this.placa = placa;
-        this.litrosGasolina = gasolina;
-        this.litrosTanqueCombustivel = litrosTanqueCombustivel;
+
+        if (litrosTanqueGasolina < 0 || litrosTanqueGasolina > 200) throw new IllegalArgumentException("Número impossível passado como 'litragem do tanque de gasolina'. Favor passar número positivo e menor que 200 litros.");
+        else this.litrosTanqueGasolina = litrosTanqueGasolina;
+
+        // Pesquisa rápida sobre como lançar erros no terminal, caso passem números impossíveis
+        if (litrosGasolina < 0 || litrosGasolina > litrosTanqueGasolina) {
+            throw new IllegalArgumentException("Número impossível passado como 'litragem de gasolina'. Favor passar número positivo e menor que a litragem do tanque.");
+        }
+        else this.litrosGasolina = litrosGasolina;
     }
 
-    public void abastecerGasolina(double litragem) {
-        litrosGasolina = litrosGasolina + litragem;
+    // Função exclusiva para debug
+    /*public void info(){
+        System.out.println("\nDono: " + dono + " | Placa: " + placa + " | Gasolina: " + litrosGasolina + " L | Litragem tanque: " + litrosTanqueGasolina + " L");
+    }*/
+
+    public void abastecerGasolina(double litrosGasolina) {
+        // Ok, aqui seguindo a ideia de que "o professor ainda não passou try and catch", então vou só fazer um if else mesmo (dois pesos e duas medidas, eu sei, mas retornar um erro parece mais legal ;P)
+        if (litrosGasolina <= 0) {
+            throw new IllegalArgumentException("Número impossível passado como 'litragem para abastecer'. Favor passar número positivo.");
+        }
+        else if (litrosGasolina > litrosTanqueGasolina - this.litrosGasolina) {
+            System.out.println("Litragem passada para abastecimento maior do que a quantidade necessária para encher o tanque. Ao invés disso, enchendo o tanque com " + (litrosTanqueGasolina - this.litrosGasolina) + " L.");
+            this.litrosGasolina = litrosTanqueGasolina;
+        }
+        else {
+            System.out.println("O veículo foi abastecido com " + litrosGasolina + " L.");
+            this.litrosGasolina = this.litrosGasolina + litrosGasolina;
+        }
     }
 
-    public void consumirGasolina(double litragem) {
-        litrosGasolina = litrosGasolina - litragem;
+    public void consumirGasolina(double litrosGasolina) {
+        if (litrosGasolina <= 0) {
+            throw new IllegalArgumentException("Número impossível passado como 'litragem para consumir'. Favor passar número positivo.");
+        }
+        else if (litrosGasolina > this.litrosGasolina) {
+            System.out.println("Litragem passada para consumo maior do que a quantidade disponível no tanque. Ao invés disso, consumindo os " + (this.litrosGasolina) + " L restantes.");
+            this.litrosGasolina = 0;
+        }
+        else {
+            System.out.println("O veículo teve " + litrosGasolina + " L consumidos de gasolina.");
+            this.litrosGasolina = this.litrosGasolina - litrosGasolina;
+        }
+
+        if (this.litrosGasolina == 0) {
+            System.out.println("Atenção!!! O veículo entrou em pane seca (0 L), quem diria que aquele contador do nível da gasolina serve para alguma coisa, hein? Agora... se vira!");
+        }
+        else if (this.litrosGasolina <= 5) {
+            System.out.println("Atenção! O veículo entrou na reserva (5 L), diriga-se imediatamente para um posto de gasolina!");
+        }
+
     }
 
-    public String getIndividuo() {
-        return individuo;
+    public String getDono() {
+        return dono;
     }
 
-    private void setIndividuo(String individuo) {
-        this.individuo = individuo;
+    private void setDono(String dono) {
+        this.dono = dono;
     }
 
     public String getPlaca() {
@@ -47,8 +88,8 @@ public class veiculo {
         this.litrosGasolina = litrosGasolina;
     }*/
 
-    public double getLitrosTanqueCombustivel() {
-        return litrosTanqueCombustivel;
+    public double getLitrosTanqueGasolina() {
+        return litrosTanqueGasolina;
     }
 
     // função não necessária no nível que temos agora, mas será chamada no constructor mais tarde no curso
